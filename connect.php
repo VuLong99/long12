309e022ce2be0a1a46a871379beabe19f4b5b90e
VuLong99 <98072540+VuLong99@users.noreply.github.com>
2022-04-16 09:27:27 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php 
  $servername = "3.132.234.157";
- $username ="dinhlong";
- $password="dinhlong";
+ $username ="btec";
+ $password="123@123a";
  $database = "tunesource";
   //Khai báo biến để kết nối đến CSDL
  $connect = mysqli_connect($servername, $username,$password,$database);
